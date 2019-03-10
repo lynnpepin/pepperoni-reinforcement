ef9e957704dfc39b596efaa755170b0d9b457c8a
Lynn Pepin <tpepin96@gmail.com>
2019-03-10 18:34:16 -0400

    Documented State

--- reinforcement_utils.py
+++ reinforcement_utils.py
@@ -19,6 +19,38 @@ def _check_number_and_geqzero(x):
         raise ValueError("Not greater than zero!")
 
 class State():
+    """Stores environmental state of our RL agent. A glorified dataclass.
+    
+    
+    # Arguments
+        All arguments are an observation of the state of the bridge.
+        r_B: 1d numpy array. Radii of leading dancers / boundary circles
+        r: 1d numpy array. Radii of all circles. |r| <= |r_B|
+        stress: Number, the stress/sigma of the bridge.
+        mass: Number, the mass of the bridge.
+        grad_mass: 1d numpy array, same size as r. Gradient of Mass w.r.t. r.
+        points: 2d numpy array, shape (*,2). Represents x,y positions.
+        edge_lengths: 1d numpy array of the edge lengths between the points.
+        angles: 1d numpy array represnting the angles of all incident edges.
+            (Note that this geographic information is unstructured.)
+    
+    # Attributes
+        _r_B, _r, _stress, _mass, _grad_mass, _edge_lengths, _angles, _points:
+        All attributes as defined in # Arguments, or None if not initialized.
+    
+    # Methods
+        Getters and _setters for each attribute,
+        _set_state(...) to set each attribute at once. Returns self.
+        Consider: A .copy() attribute, returning State
+    
+    # Examples
+        # Initialize an empty state and set some basic values
+        state = reinforcement_utils.State()
+        state._set_mass(300)
+        state._set_r(np.array([1,1,1,3]))
+        # Same thing, but a one liner
+        state = reinforcement_utils.State(mass = 300, r = np.array([1,1,1,3]))
+    """
     def __init__(self, r_B          = None,
                        r            = None,
                        stress       = None,
@@ -41,27 +73,35 @@ class State():
     
     # Getters and setters
     def get_r_B(self):
+        """Getter for 1d np array r_B"""
         return self._r_B
     
     def get_r(self):
+        """Getter for 1d np array r"""
         return self._r
     
     def get_stress(self):
+        """Getter for number stress"""
         return self._stress
     
     def get_mass(self):
+        """Getter number mass"""
         return self._mass
     
     def get_grad_mass(self):
+        """Getter for 1d np array grad_mass"""
         return self._grad_mass
     
     def get_edge_lengths(self):
+        """Getter for 1d np array edge_lengths"""
         return self._edge_lengths
     
     def get_angles(self):
+        """Getter for 1d np array angles"""
         return self._angles
     
     def get_points(self):
+        """Getter for 2d np array points of shape (*,2)"""
         return self._points
 
     def _set_r_B(self,r_B):
@@ -139,4 +179,5 @@ class State():
         if grad_mass is not None:    self._grad_mass = grad_mass
         if edge_lengths is not None: self._edge_lengths = edge_lengths
         if angles is not None:       self._angles    = angles
-        if points is not None:       self._points    = points    
+        if points is not None:       self._points    = points
+        return self
