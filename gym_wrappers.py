968e6129b8881e980280ef301ceaed6f2b7ff34e
Lynn Pepin <tpepin96@gmail.com>
2019-04-03 16:13:15 -0400

    Copied/pasted documentation to Env. Time to get cracking

--- gym_wrappers.py
+++ gym_wrappers.py
@@ -64,8 +64,20 @@ def observation_space_dict(ld_length = 10):
 class BHDEnv(gym.Env):
     """
     BridgeHoleDesign environment.
+    See: https://github.com/openai/gym/blob/master/gym/core.py
+        (Documentation shamelessly stolen from there.)
     
-    
+    Attributes:
+        action_space: The Space object corresponding to valid actions
+        observation_space: The Space object corresponding to valid observations
+        reward_range: A tuple corresponding to the min and max possible rewards
+        
+    Methods:
+        step:  Mapping action --> observation, reward, done, info 
+        reset:  Returns observation of initial state of the space.
+        render:  Renders the environment. Mode == 'human', 'rgb_array', or 'ansi' by convention.
+        close:  Automatically run when garbage collection / exit.
+        seed:  Set the seed for the environment's RNG/RNGs. Returns list of seed history.
     """
     
     def __init__(self, bridge = None,
@@ -96,19 +108,66 @@ class BHDEnv(gym.Env):
         self.observation_space = observation_space_dict(ld_length = ld_length)
     
     def step(self, action):
-        # action = vector
+        """Accepts an action and returns a tuple (observation, reward, done, info).
+        
+        Argumentss:
+            action (object): an action provided to the environment
+            
+        Returns:
+            observation (object): agent's observation of the current environment
+            reward (float) : amount of reward returned after previous action
+            done (boolean): whether the episode has ended, in which case further step() calls will return undefined results
+            info (dict): auxiliary diagnostic information for debugging, logging.
+        """
         raise NotImplementedError
     
     def reset(self):
+        """Resets the state of the environment and returns an initial observation.
+        Returns: observation (object): the initial observation of the
+            space.
+        """
         raise NotImplementedError
 
     def render(self, mode='human'):
+        """Renders the environment.
+        
+        The set of supported modes varies per environment. (And some
+        environments do not support rendering at all.)
+        
+        Conventional modes:
+        - human: render to the current display or terminal.
+        - rgb_array: Return an numpy.ndarray with shape (x, y, 3).
+        - ansi: Return a string (str) or StringIO.StringIO containing a
+          terminal-style text representation.
+        
+        Arguments:
+            mode (str): the mode to render with
+        """
         raise NotImplementedError
 
     def close(self):
+        """Override _close in your subclass to perform any necessary cleanup.
+        
+        Environments will automatically close() themselves when
+        garbage collected or when the program exits.
+        """
         raise NotImplementedError
 
     def seed(self, seed=123456789):
+        """Sets the seed for this env's random number generator(s).
+        
+        Note:
+            Some environments use multiple pseudorandom number generators.
+            We want to capture all such seeds used in order to ensure that
+            there aren't accidental correlations between multiple generators.
+            
+        Returns:
+            list<bigint>: Returns the list of seeds used in this env's random
+              number generators. The first value in the list should be the
+              "main" seed, or the value which a reproducer should pass to
+              'seed'. Often, the main seed equals the provided 'seed', but
+              this won't be true if seed=None, for example.
+        """
         raise NotImplementedError
     
 
