f70f19aea71b6a244d27ff4d67218a8e5a059ea3
Lynn Pepin <tpepin96@gmail.com>
2019-04-03 20:33:09 -0400

    Still not working -- need to rework the gym wrapper to output a Box, not a Dict.

--- example_rl.py
+++ example_rl.py
@@ -1,40 +1,51 @@
+# See: https://github.com/keras-rl/keras-rl/blob/master/examples/dqn_cartpole.py
+
 import numpy as np
 import gym
 from gym_wrappers import BHDEnv
-
-# See: https://github.com/keras-rl/keras-rl/blob/master/examples/dqn_cartpole.py
-
 from keras.models import Sequential
-from keras.layers import Dense, Activation, Flatten
+from keras.layers import Dense, Activation, Flatten, Input
 from keras.optimizers import Adam
-
 from rl.agents.dqn import DQNAgent
 from rl.policy import BoltzmannQPolicy
 from rl.memory import SequentialMemory
 
 
+# Set up environment
 env = BHDEnv(length = 20.0, height = 10.0, allowable_stress = 200.0)
 ob = env.reset()
 np.random.seed(123)
 env.seed(123)
+
+
+# Input shape
+# TODO - This will still be size (34,), or maybe (1,)? But can no longer be a dictionary...
+obs = env.observation_space
+space_shapes = [obs.spaces[key].shape for key in obs.spaces.keys()]
+#e.g. spaces_shapes = [(10,), (1,), (1,), (10, 2), (1,), (1,)]
+input_shape = (np.sum([np.prod(shape) for shape in space_shapes]), )
+
+# Output shape
 ld_length = env.action_space.shape[0]
 
+
+# Set up neural network, RL stuff
 model = Sequential()
-# TODO - Hope the model can Flatten input spaces.Dict,
-#        and learn the input shape automatically.
-#        Else, we'll need to convert the dict to a numpy array / shapes.Box
-model.add(Flatten())
-model.add(Dense(32))
+model.add(Dense(32, input_shape = input_shape))
 model.add(Activation('relu'))
 model.add(Dense(ld_length))
 
 memory = SequentialMemory(limit=50, window_length=1)
 policy = BoltzmannQPolicy()
+
 dqn = DQNAgent(model=model, nb_actions=ld_length, memory=memory, nb_steps_warmup=3,
                target_model_update=1e-2, policy=policy)
+
 dqn.compile(Adam(lr=1e-3), metrics=['mae'])
 
-dqn.fit(env, nb_steps=50, visualize=False, verbose=2)
+
+# And fit!
+dqn.fit(env, nb_steps=3, visualize=False, verbose=2)
 
 # After training is done, we save the final weights.
 dqn.save_weights('dqn_{}_weights.h5f'.format(ENV_NAME), overwrite=True)
