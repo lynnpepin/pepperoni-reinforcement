b45aa4869fce7aabc474c44981d0212cd4781f92
Lynn Pepin <tpepin96@gmail.com>
2019-04-04 10:07:50 -0400

    Progress made -- shapes work properly, but Value error during fitting.

--- example_rl.py
+++ example_rl.py
@@ -18,15 +18,11 @@ np.random.seed(123)
 env.seed(123)
 
 
-# Input shape
-# TODO - This will still be size (34,), or maybe (1,)? But can no longer be a dictionary...
+# Input shape and output shape
 obs = env.observation_space
-space_shapes = [obs.spaces[key].shape for key in obs.spaces.keys()]
-#e.g. spaces_shapes = [(10,), (1,), (1,), (10, 2), (1,), (1,)]
-input_shape = (np.sum([np.prod(shape) for shape in space_shapes]), )
-
-# Output shape
 ld_length = env.action_space.shape[0]
+input_shape = (1,) + obs.shape
+#input_shape = (1,) * (ld_length - 1) + obs.shape
 
 
 # Set up neural network, RL stuff
@@ -34,21 +30,19 @@ model = Sequential()
 model.add(Dense(32, input_shape = input_shape))
 model.add(Activation('relu'))
 model.add(Dense(ld_length))
+model.add(Flatten())
+
 
 memory = SequentialMemory(limit=50, window_length=1)
 policy = BoltzmannQPolicy()
 
 dqn = DQNAgent(model=model, nb_actions=ld_length, memory=memory, nb_steps_warmup=3,
                target_model_update=1e-2, policy=policy)
-
 dqn.compile(Adam(lr=1e-3), metrics=['mae'])
 
 
 # And fit!
-dqn.fit(env, nb_steps=3, visualize=False, verbose=2)
+dqn.fit(env, nb_steps=2, visualize=False, verbose=2)
 
-# After training is done, we save the final weights.
 dqn.save_weights('dqn_{}_weights.h5f'.format(ENV_NAME), overwrite=True)
-
-# Finally, evaluate our algorithm for 5 episodes.
 dqn.test(env, nb_episodes=5, visualize=True)
